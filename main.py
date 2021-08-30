Fix: Vector calculation and display

main.py
@@ -12,6 +12,7 @@ CELL_SIZE = 20
 
 z = -2
 angulo_rot = 1
+
 def draw():
     
     global z, angulo_rot
@@ -20,24 +21,45 @@ def draw():
     glPushMatrix()
 
     # vec = Vector(1,1, size=100)
-    # vec = Vector(0,9, size=6)
+    # vec = Vector(2,-9, size=200)
     
     # V = VectorField(10, -600, -600, 600, 600)
-    V = VectorField(lambda x,y: x+y, lambda x,y:x+y, 21)
-    # print(V.coordinates)
-    # Vector(angle=angulo_rot, size=1)
+    func_x = lambda x,y: x**2 - y**2 - 100
+    func_y = lambda x,y: 2*x*y
+    # V = VectorField(func_x, func_y, 51,vectors=True)
 
-    # draw_axes(5,5,5)
-    # draw_axes(600,600,600)
-    # draw_pontos(N_PONTOS, 800,-800,800,-800)
-
-    # draw_grid(draw_vector, N_PONTOS, 5,-5,5,-5)
-    # draw_vector(1,1, angle =45, size=.5)
-
+    # Vx(x,y)=(-3x) / sqrt(x x + y y); Vy(x,y)=(-3y)/ sqrt(x x + y y);
+    # func_x = lambda x,y: -3*x /np.sqrt(x**2 + y**2)
+    # func_y = lambda x,y: -3*y /np.sqrt(x**2 + y**2)
+    # V = VectorField(func_x, func_y, 15,vectors=True)
+    
+    # c) Vx(x,y)=4cos(y / 3 + p / 4); Vy(x,y)=4sin(x / 3 + p / 4) 
+    func_x = lambda x,y: 4 * np.cos((y/3)+(x/4))
+    func_y = lambda x,y: 4 * np.sin((x/3)+(y/4))
+    V = VectorField(func_x, func_y, 20,vectors=True)
+    # V = VectorField(lambda x, y: x, lambda x, y: -y, 31,vectors=True)
+    
+    # curve(V, 1, 1, 50, 15)
+    
     glPopMatrix()
     glutSwapBuffers() #Não sei o que faz
 
     angulo_rot += 2
+
+def curve(V, x,y, step, num_steps):
+    p = np.array([x,y])
+
+    glBegin(GL_LINES)
+    glColor3f(255,0,0)
+    glVertex3f(*p,0)
+    for i in range(num_steps):
+        vector = V.get_vector_from_pos(x,y)
+        new_p = p + (vector * step)
+        if new_p is not None:
+            glVertex3f(*new_p,0)
+
+    glEnd()       
+
 
 
 def vector(v,size):

vectorField.py
@@ -5,7 +5,7 @@ import numpy as np
 from vector import Vector
 
 class VectorField():
-    def __init__(self, func_x , func_y,  n_points=10, x0=None, y0=None, xf=None, yf=None, axes=True, points=True, square=True):
+    def __init__(self, func_x , func_y,  n_points=10, x0=None, y0=None, xf=None, yf=None, axes=True, points=False, vectors=True):
         # Canvas
         coordinates = [x0, xf, y0, yf]
         if all(coordinates):
@@ -27,30 +27,44 @@ class VectorField():
             raise Exception("Must pass n_points or cell size.")
 
         # Grid
-        self.grid_values = np.zeros((n_points, n_points))
+        self.grid_values = np.zeros((n_points, n_points, 2))
 
         self.func_x = func_x
         self.func_y = func_y
 
+        self._update_field()
+
         if axes:
             self._draw_axes()
-
-        self.draw()
+        if vectors:
+            self._draw_vectors()
+        if points:
+            self._draw_points()
         # self._draw_points()
 
     def _update_field(self):
-        xi, yi = np.meshgrid(
-            np.arange(-self.n_points/2, (self.n_points/2)+1),
-            np.arange(-self.n_points/2, (self.n_points/2)+1),
-            indexing='ij')
-        
-        x = self.func_x(xi[::-1], yi)
-        y = self.func_y(xi[::-1], yi)
+        # xi, yi = np.meshgrid(
+        #     np.arange((-self.n_points//2)+1, self.n_points//2),
+        #     np.arange((-self.n_points//2)+1, self.n_points//2),
+        #     indexing='ij')
 
-        return np.dstack((x,y)) 
+        # # x = self.func_x(xi[::-1], yi)
+        # # y = self.func_y(xi[::-1], yi)
+        # coord = np.dstack((xi[::-1],yi))
+
+        for i, array in enumerate(self.grid_values):
+            for j, vector in enumerate(array):
+                x = j - (self.n_points//2)
+                y = i - (self.n_points//2) 
+                
+                new_x = self.func_x(x, y)
+                new_y = self.func_y(x, y) 
+                self.grid_values[i][j] = np.array([new_x, new_y])
+
+        
+        return  
     
-    def draw(self):
-        # self.grid_values = self._update_field()      
+    def _draw_vectors(self):      
         (x0, y0), _ = self.coordinates
 
         for i, array in enumerate(self.grid_values):
@@ -58,20 +72,20 @@ class VectorField():
                 offset = np.array([self.dx/2, self.dy/2])
                 cell_pos = np.array([x0 + (self.dx * j), y0 + (self.dy * i)])
                 pos = cell_pos + offset
-                # print(vector, pos)
-                Vector(1,0,position=pos, size=self.dx - 15)
-                # Vector(vector[0],vector[1],position=pos, size=self.dx - 15)
+                vector = self.grid_values[i][j]
+                Vector(*vector,position=pos, size=self.dx *.8)
 
     def _draw_points(self):
-        # self.grid_values = self._update_field()      
         (x0, y0), _ = self.coordinates
 
         for i, array in enumerate(self.grid_values):
             for j, vector in enumerate(array):
-                pos = (x0 + (self.dx * j), y0 + (self.dy * i))
-                # print(vector, pos)
+                offset = np.array([self.dx/2, self.dy/2])
+                cell_pos = np.array([x0 + (self.dx * j), y0 + (self.dy * i)])
+                pos = cell_pos + offset
                 
                 glBegin(GL_POINTS)
+                glColor3fv((255,255,255))
                 glVertex3f(pos[0],pos[1],0)
                 glEnd()
                 # Vector(1,0,position=pos, size=self.dx - 15)
@@ -79,7 +93,7 @@ class VectorField():
     
     def _draw_axes(self):
         (x0, y0), (xf, yf) = self.coordinates
-
+    
         x_mid = ((xf-x0)/2) + x0
         y_mid = ((yf-y0)/2) + y0
 
@@ -97,3 +111,14 @@ class VectorField():
         for i in range(len(axes_points)):
             glVertex3fv(axes_points[i])
         glEnd()
+    
+    def get_vector_from_pos(self, x, y):
+        (x0, y0), (xf, yf) = self.coordinates
+        
+        if (x > x0 and x < xf) and (y > y0 and y < yf):
+            i = int(np.ceil(y /self.dy))
+            j = int(np.ceil(x /self.dx))
+
+            return self.grid_values[i][j]
+        else:
+            return None